25bb86e5fd40478fa022779dd37db56ee9da997c
Lizhiyu599 <lizhiyu807@gmail.com>
2026-06-23 10:24:07 +0800

    Update chat_core.js

--- js/chat_core.js
+++ js/chat_core.js
@@ -350,7 +350,7 @@ if (transferMatch) {
 }
 
 // ===== 检测角色旁白发表情包 =====
-    var emojiAllow = ChatConfig?.settings?.emojiAllow !== false;
+    var emojiAllow = (ChatConfig && ChatConfig.settings && ChatConfig.settings.emojiAllow) !== false;
     var emojiMatch = cleanContent.match(/[\(\（]([^\)\）]*)发送了表情包[：:]\s*([^\)\）]+)[\)\）]/);
     if (emojiAllow && emojiMatch && emojiMatch[2])
     var emojiNote = emojiMatch[2].trim();
