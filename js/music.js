a46ca87cf1c084b66a34555af81f447e090a0666
Lizhiyu599 <lizhiyu807@gmail.com>
2026-07-02 21:18:41 +0800

    Update music.js

--- js/music.js
+++ js/music.js
@@ -1401,13 +1401,14 @@ function playSong(songId) {
 if (!musicCurrentSong.lyrics || musicCurrentSong.lyrics.length === 0) {
     var name = musicCurrentSong.name || '歌曲';
     musicCurrentSong.lyrics = [
-        { time: 0, text: '♪ ' + name + ' ♪' },
-        { time: 5, text: '歌词功能已上线' },
-        { time: 10, text: '点击歌词可跳转' },
-        { time: 15, text: '随播放自动滚动' },
-        { time: 20, text: '长按收藏暂未开放' },
-        { time: 25, text: '敬请期待更多功能' }
-    ];
+    { time: 0, text: '♪ ' + name + ' ♪' },
+    { time: 5, text: '右上角菜单可' },
+    { time: 10, text: '改音乐封面' },
+    { time: 15, text: '歌名歌手名' },
+    { time: 20, text: '和角色一起听' },
+    { time: 25, text: '编辑歌词' },
+    { time: 30, text: '自定义唱片' }
+];
 }
     
     if (song.type === 'local') {
